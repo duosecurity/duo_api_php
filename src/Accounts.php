56c8ae8c61b150239568113413d5e15893ae2020
Matt Schwager <schwag09@gmail.com>
2015-08-03 14:20:39 -0400

    Accounts API.

---
+++ src/Accounts.php
@@ -0,0 +1,46 @@
+<?php
+namespace DuoAPI;
+
+/*
+ * https://www.duosecurity.com/docs/accountsapi
+ */
+
+require_once("Client.php");
+
+class Accounts extends Client {
+
+    public function list() {
+        $method = "POST";
+        $endpoint = "/accounts/v1/account/list";
+        $params = array();
+
+        return self::jsonApiCall($method, $endpoint, $params);
+    }
+
+    public function create($name) {
+        assert('is_string($name)');
+
+        $method = "POST";
+        $endpoint = "/accounts/v1/account/create";
+        $params = array(
+            "name" => $name,
+        );
+
+        return self::jsonApiCall($method, $endpoint, $params);
+    }
+
+    public function delete($account_id) {
+        assert('is_string($account_id)');
+
+        $method = "POST";
+        $endpoint = "/accounts/v1/account/delete";
+        $params = array(
+            "account_id" => $account_id,
+        );
+
+        return self::jsonApiCall($method, $endpoint, $params);
+    }
+
+}
+
+?>
